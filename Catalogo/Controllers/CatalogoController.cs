890cea16fd15c93b3d153532db8d347f6277a716
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2020-12-09 09:07:44 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo/Controllers/CatalogoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Catalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogoController : ControllerBase
+    {
+        // GET: api/<CatalogoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Produto 1", "Produto 2" };
+        }
+    }
+}
